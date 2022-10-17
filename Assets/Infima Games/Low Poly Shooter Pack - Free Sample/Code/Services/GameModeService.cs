00bd43f6940f64e0a0848e573ae7843f80f82fd9
Finn W <35266822+JediKnight122@users.noreply.github.com>
2022-10-17 14:07:19 +0200

    Initial Commit

---
+++ Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Services/GameModeService.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2021, Infima Games. All Rights Reserved.
+
+namespace InfimaGames.LowPolyShooterPack
+{
+    /// <summary>
+    /// Game Mode Service.
+    /// </summary>
+    public class GameModeService : IGameModeService
+    {
+        #region FIELDS
+        
+        /// <summary>
+        /// The Player Character.
+        /// </summary>
+        private Character playerCharacter;
+        
+        #endregion
+        
+        #region FUNCTIONS
+        
+        public Character GetPlayerCharacter()
+        {
+            //Make sure we have a player character that is good to go!
+            if (playerCharacter == null)
+                playerCharacter = UnityEngine.Object.FindObjectOfType<Character>();
+            
+            //Return.
+            return playerCharacter;
+        }
+        
+        #endregion
+    }
+}
